b9cc6063477cfc6f622ff9849255ee96bc1af4f0
AamirCode2 <aamirlovesart@gmail.com>
2025-09-25 19:44:37 +1000

    Add files via upload

---
+++ square pro.py
@@ -0,0 +1,25 @@
+import turtle 
+
+turtle.Screen().bgcolor("Aqua")
+turtle.Screen().setup(500,600)
+
+pen = turtle.Turtle()
+
+pen.forward(200)
+
+turtle.Screen().bgcolor("Green")
+
+pen.right(90)
+pen.forward(200)
+
+turtle.Screen().bgcolor("Orange")
+
+pen.right(90)
+pen.forward(200)
+
+turtle.Screen().bgcolor("Red")
+
+pen.right(90)
+pen.forward(200)
+
+turtle.Screen().bgcolor("Purple")
